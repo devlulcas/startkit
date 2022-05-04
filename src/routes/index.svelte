35ba4ad36d46d3c1e5129758da140cff9c5ce1e4
lucas rego <lucasalvesrego.contato@gmail.com>
2022-05-04 14:49:08 -0300

    feat: placeholders

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Apps from '$lib/Apps.svelte';
+	import Widgets from '$lib/Widgets.svelte';
+</script>
+
+<Widgets />
+<Apps />
