6bb51ce7fd56894ed7fa5cc502157c219b90a0a7
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-07 23:09:26 -0300

    feat: global gradient var

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	import '../app.css';
-	import NavBar from '$lib/NavBar.svelte';
+	import "../app.css";
+	import NavBar from "$lib/NavBar.svelte";
 </script>
 
 <NavBar />
 <slot />
+
+<style>
+	:global(.lc-gradient) {
+		background-image: linear-gradient(0deg, #5b21b6, #a855f7);
+	}
+</style>
