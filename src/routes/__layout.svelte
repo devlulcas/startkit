a8c9d31fdd1a1c52ca5ba4b714896fa74aaa26f8
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-12 01:07:36 -0300

    feat: better global styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,8 +6,14 @@
 <NavBar />
 <slot />
 
-<style>
+<style lang="postcss">
 	:global(.lc-gradient) {
-		background-image: linear-gradient(0deg, #5b21b6, #a855f7);
+		@apply from-violet-800 to-violet-600 bg-gradient-to-b text-violet-50;
+	}
+
+	:global(body) {
+		@apply bg-gray-700;
+
+		background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23737373' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 	}
 </style>
