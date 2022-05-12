1d033a10347a387e0e40f3a4e0e33f1413714eef
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-12 01:06:54 -0300

    feat: holder for widgets

---
+++ src/lib/Widgets.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import ColorConverter from "./ColorConverter.svelte";
+	import Pet from "./Pet.svelte";
+	import Pomodoro from "./Pomodoro.svelte";
+</script>
+
+<div class="px-4 py-2">
+	<div class="flex w-full gap-3">
+		<ColorConverter />
+		<Pet />
+		<Pomodoro />
+	</div>
+</div>
