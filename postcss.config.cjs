ea8b5b9844295186b93e9d7a264dde76d011d0ec
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-12 00:59:57 -0300

    deps(npm): add fontawesome and postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const postcssNesting = require("postcss-nesting");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+	plugins: [tailwindcss, autoprefixer, postcssNesting],
+};
