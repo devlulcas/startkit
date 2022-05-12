9301f7e0ac33329c6248b671e3433e55944d39b8
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-12 01:05:18 -0300

    refactor: moved styles to at-apply

--- src/lib/NavItem.svelte
+++ src/lib/NavItem.svelte
@@ -1,3 +1,9 @@
 <li class="grow">
-	<a class="text-slate-800 rounded text-center block w-full bg-violet-50 hover:bg-violet-200 text-2xl font-bold" href="">LOREM</a>
+	<a href="#">LOREM</a>
 </li>
+
+<style lang="postcss">
+	a {
+		@apply text-slate-800 p-2 rounded text-center block w-full bg-opacity-20 bg-white backdrop-blur-sm hover:bg-violet-800 hover:bg-opacity-60 text-2xl font-bold text-white;
+	}
+</style>
